04119d576f000ec18130f136588c948f37198355
samprakash.R <samprakash1410@gmail.com>
2024-05-02 11:52:33 +0530

    modifiy

--- investment.py
+++ investment.py
@@ -1,10 +1,24 @@
 import argparse
 from calculators.lumpsum import returns as lumpsum_returns
 from calculators.sip import returns as sip_returns
+from decorators.storage import store_in_csv
+
+@store_in_csv
+def print_result(principal, time, rate, investment_type,future_value):
+    """
+    This method prints the result
+    """
+    print(
+        f"For Your {investment_type} the  {principal} will be {future_value} in next {time} years"
+        )
 
 
 def argument_parser():
-	@@ -15,7 +16,7 @@ def argument_parser():
+    """
+    This method creates an argument parser
+    """
+    parser = argparse.ArgumentParser(
+        description="Investment Calculator")
     parser.add_argument(
         'investment_type',
         type=str,
@@ -12,7 +26,13 @@ def argument_parser():
         help="investment type"
         )
     parser.add_argument(
-	@@ -29,7 +30,7 @@ def argument_parser():
+        '-p',
+        '--principal', 
+        type=int,
+        required=True,
+        help="present value of investment")
+    parser.add_argument(
+        '-r',
         '--rate',
         type=int,
         required=True,
@@ -20,7 +40,10 @@ def argument_parser():
     parser.add_argument(
         '-t',
         '--time',
-	@@ -41,9 +42,16 @@ def argument_parser():
+        type=int,
+        required=True,
+        help="time in years")
+    return parser
 if __name__ == "__main__":
     args = argument_parser().parse_args()
     if args.investment_type == 'lumpsum':
@@ -29,11 +52,11 @@ if __name__ == "__main__":
             intrest_rate=args.rate,
             time_in_years=args.time
         )
-        print(f"Your investment of {args.principal} will be {result} in next {args.time} years")
+        print_result(args.principal, args.time, args.rate, args.investment_type, result)
     elif args.investment_type == 'sip':
         result = sip_returns(
             invested_amount = args.principal,
             return_rate=args.rate,
             total_period_years=args.time
         )
-        print(f"Your investment of {args.principal} will be {round(result,2)} in next {args.time} years")
+        print_result(args.principal, args.time, args.rate, args.investment_type,result)
